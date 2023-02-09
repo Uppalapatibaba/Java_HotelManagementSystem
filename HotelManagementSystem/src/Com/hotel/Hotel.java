73f26f271e0e0882337b58abdf69c4f422387d5d
Uppalapatibaba <babadasthagiri83@gmail.com>
2023-02-09 18:37:59 +0530

    second_commit

--- HotelManagementSystem/src/Com/hotel/Hotel.java
+++ HotelManagementSystem/src/Com/hotel/Hotel.java
@@ -77,7 +77,7 @@ class Doubleroom extends Singleroom implements Serializable
 	}
 }
 
-class NotAvaiable extends Exception
+class NotAvailable extends Exception
 {
 	public String toString()
 	{
@@ -97,7 +97,7 @@ class Reception
 {
 	static Holder hotel_ob = new Holder();
 	static Scanner input = new Scanner(System.in);
-	
+
 	static void CustDetails(int i, int rn)
 	{
 		String name, contact, gender;
@@ -105,15 +105,232 @@ class Reception
 		String gender2 = "";
 		System.out.println("\nEnter customer name: ");
 		name = input.next();
-		
+
 		System.out.println("\nEnter customer contact: ");
 		contact = input.next();
-		
-		System.out.println("\nEnter customer gender: ");
+
+		System.out.println("\nEnter gender: ");
 		gender = input.next();
+
+		if(i < 3)
+		{
+			System.out.println("Enter second customer name: ");
+			name2 = input.next();
+			System.out.println("Enter customer number: ");
+			contact2 = input.next();
+			System.out.println("Enter gender: ");
+			gender2 = input.next();
+		}
+
+		switch(i)
+		{
+		case 1:
+			hotel_ob.luxury_doubleroom[rn] = new Doubleroom(name, contact, gender, name2, contact2, gender2);
+			break;
+
+		case 2:
+			hotel_ob.deluxe_doubleroom[rn] = new Doubleroom(name, contact, gender, name2, contact2, gender2);
+			break;
+
+		case 3:
+			hotel_ob.luxury_singleroom[rn] = new Singleroom(name, contact, gender);
+			break;
+
+		case 4:
+			hotel_ob.deluxe_singleroom[rn] = new Singleroom(name, contact, gender);
+			break;
+
+		default : 
+			System.out.println("Wrong option");
+			break;
+		}
 	}
 
+	static void bookroom(int i)
+	{
+		int j;
+		int rn;
+		System.out.println("\n Please choose room number from: ");
+
+		switch(i)
+		{
+		case 1:
+			for(j = 0; j < hotel_ob.luxury_doubleroom.length; j++);
+			{
+				if(hotel_ob.deluxe_doubleroom[j] == null)
+				{
+					System.out.println(j + 1 + ",");
+				}
+			}
+			System.out.println("\nEnter room number: ");
+			try
+			{
+				rn = input.nextInt();
+				rn--;
+				if(hotel_ob.luxury_doubleroom[rn] != null)throw new NotAvailable();
+				CustDetails(i, rn);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Invalid Option");
+				return;
+			}
+			break;
+
+		case 2:
+			for(j = 0; j < hotel_ob.deluxe_doubleroom.length; j++)
+			{
+				if(hotel_ob.deluxe_doubleroom[j] == null)
+				{
+					System.out.println(j + 11 + ",");
+				}
+			}
+			System.out.println("\nEnter room number: ");
+			try
+			{
+				rn = input.nextInt();
+				rn = rn - 11;
+
+				if(hotel_ob.deluxe_doubleroom[rn] != null)throw new NotAvailable();
+				CustDetails(i, rn);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Invalid Option");
+				return;
+			}
+			break;
+
+		case 3: 
+			for(j = 0; j < hotel_ob.luxury_singleroom.length; j++)
+			{
+				if(hotel_ob.luxury_singleroom[j] == null)
+				{
+					System.out.println(j + 31 + ",");
+				}
+			}
+			System.out.println("\nEnter room number: ");
+
+			try
+			{
+				rn = input.nextInt();
+				rn = rn - 31;
+				if(hotel_ob.luxury_singleroom[rn] != null)throw new NotAvailable();
+				CustDetails(i, rn);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Invalid Option");
+				return;
+			}
+			break;
+
+		case 4:
+			for(j = 0; j < hotel_ob.deluxe_singleroom.length; j++)
+			{
+				if(hotel_ob.deluxe_singleroom[j] == null)
+				{
+					System.out.println(j + 41 + ",");
+				}
+			}
+			System.out.println("\nEnter room number: ");
+
+			try
+			{
+				rn = input.nextInt();
+				rn = rn - 41;
+				if(hotel_ob.deluxe_singleroom[rn] != null)throw new NotAvailable();
+				CustDetails(i, rn);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Invalid Option");
+				return;
+			}
+			break;
+
+		default : 
+			System.out.println("Enter valid option");
+			break;
+		}
+		System.out.println("Room Booked");
+	}
+	static void features(int i)
+	{
+		switch(i)
+		{
+		case 1: 
+			System.out.println("Number of double beds : 1\nAC: " 
+					+ "Yes\nFree breakfast : Yes\nCharge per day: " + " 4000 ");
+			break;
+
+		case 2: 
+			System.out.println("Number of double beds : 1\nAC: " 
+					+ "No\nFree breakfast : Yes\nCharge per day: " + " 3000 ");	
+			break;
+
+		case 3: 
+			System.out.println("Number of single beds : 1\nAC: " 
+					+ "Yes\nFree breakfast : Yes\nCharge per day: " + " 2200 ");	
+			break;
+
+		case 4: 
+			System.out.println("Number of single beds : 1\nAC: " 
+					+ "No\nFree breakfast : Yes\nCharge per day: " + " 1200 ");	
+			break;
+
+		default :
+			System.out.println("Enter valid option");
+			break;
+		}
+	}
+
+	static void availability(int i)
+	{
+		int j, count = 0;
+		switch(i)
+		{
+		case 1:
+			for(j = 0; j < hotel_ob.luxury_doubleroom.length; j++)
+			{
+				if(hotel_ob.luxury_doubleroom[j] == null)
+					count++;
+			}
+			break;
+
+		case 2:
+			for(j = 0; j < hotel_ob.deluxe_doubleroom.length; j++)
+			{
+				if(hotel_ob.deluxe_doubleroom[j] == null)
+					count++;
+			}
+			break;
+
+		case 3:
+			for(j = 0; j < hotel_ob.luxury_singleroom.length; j++)
+			{
+				if(hotel_ob.luxury_singleroom[j] == null)
+					count++;
+			}
+			break;
+
+		case 4:
+			for(j = 0; j < hotel_ob.deluxe_singleroom.length; j++)
+			{
+				if(hotel_ob.deluxe_singleroom[j] == null)
+					count++;
+			}
+			break;
+		default :
+			System.out.println("Enter valid option");
+			break;
+		}
+		System.out.println("Number of rooms available : " + count);
+	}
+	
 }
+
+
 public class Hotel 
 {
 	public static void main(String[] args) 
